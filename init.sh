b7037d5c1c0b7da51a7bca96dd9071326b47476a
Chris Phillipson <cphillipson@pivotal.io>
2024-04-23 21:36:53 -0700

    The hits keep coming

--- init.sh
+++ init.sh
@@ -93,8 +93,8 @@ main() {
     "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
     $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  sudo apt-get update
-  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  sudo apt update
+  sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
   sudo groupadd docker
   sudo usermod -aG docker ubuntu
   newgrp docker
